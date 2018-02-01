9d606ae9f906379b16b6c9359b2554486e818a6b
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2018-01-31 22:18:25 -0500

    Fix build

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,7 @@
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
 extern crate rand;
-extern crate serde;
-extern crate serde_json;
-
-#[macro_use]
-extern crate serde_derive;
 
 mod util;
 pub mod data_structures;
 pub mod hash;
-pub mod kademlia;
