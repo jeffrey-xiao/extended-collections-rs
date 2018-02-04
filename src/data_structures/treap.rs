83b5bd4f92ec66e4c3b37a7adc22dc43abeeed8a
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2018-02-04 02:49:32 -0500

    Add documentation

--- src/data_structures/treap.rs
+++ src/data_structures/treap.rs
@@ -2,6 +2,7 @@ use std::vec::Vec;
 use rand;
 use util;
 
+/// A struct representing an internal node of a treap.
 struct Node <T: PartialOrd, U> {
     key: T,
     value: U,
@@ -13,9 +14,47 @@ struct Node <T: PartialOrd, U> {
 
 type Tree<T, U> = Option<Box<Node<T, U>>>;
 
+/// An ordered map implemented by a treap.
+///
+/// A treap is a tree that satisfies both the binary search
+/// tree property and a heap property. Each node has a key, a value, and a priority. The key of any
+/// node is greather than all keys in its left subtree and less than all keys occuring in its right
+/// subtree. The priority of a node is greater than the priority of all nodes in its subtrees. By
+/// randomly generating priorities, the expected height of the tree is proportional to the
+/// logarithm of the number of keys.
+///
+/// # Examples:
+/// ```
+/// use code::data_structures::Treap;
+///
+/// let mut t = Treap::new();
+/// t.insert(0, 1);
+/// t.insert(3, 4);
+///
+/// assert_eq!(t.get(&0), Some(&1));
+/// // a non-existent key will yield None
+/// assert_eq!(t.get(&1), None);
+/// assert_eq!(t.size(), 2);
+///
+/// assert_eq!(t.min(), Some(&0));
+/// assert_eq!(t.ceil(&2), Some(&3));
+///
+/// *t.get_mut(&0).unwrap() = 2;
+/// assert_eq!(t.remove(&0), Some((0, 2)));
+/// // a non-existent key will yield None and not throw an error
+/// assert_eq!(t.remove(&1), None);
+/// ```
 pub struct Treap<T: PartialOrd, U>(Tree<T, U>);
 
 impl<T: PartialOrd, U> Treap<T, U> {
+    /// Constructs a new, empty `Treap<T, U>`
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t: Treap<u32, u32> = Treap::new();
+    /// ```
     pub fn new() -> Self { Treap(None) }
 
     fn update(tree: &mut Tree<T, U>) {
@@ -101,6 +140,19 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
+    /// Inserts a key-value pair into the treap. If the key already exists in the treap, it will
+    /// return and replace the old key-value pair.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// assert_eq!(t.insert(1, 1), None);
+    /// assert_eq!(t.get(&1), Some(&1));
+    /// assert_eq!(t.insert(1, 2), Some((1, 1)));
+    /// assert_eq!(t.get(&1), Some(&2));
+    /// ```
     pub fn insert(&mut self, key: T, value: U) -> Option<(T, U)> {
         let &mut Treap(ref mut tree) = self;
 
@@ -125,6 +177,18 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
+    /// Removes a key-value pair from the treap. If the key exists in the treap, it will return
+    /// the associated key-value pair. Otherwise it will return `None`.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// assert_eq!(t.remove(&1), Some((1, 1)));
+    /// assert_eq!(t.remove(&1), None);
+    /// ```
     pub fn remove(&mut self, key: &T) -> Option<(T, U)> {
         let &mut Treap(ref mut tree) = self;
         let (old_node_opt, r_tree) = Self::split(tree, key);
@@ -138,66 +202,111 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
-    fn tree_contains(tree: &Tree<T, U>, k: &T) -> bool {
+    fn tree_contains(tree: &Tree<T, U>, key: &T) -> bool {
         match *tree {
             Some(ref node) => {
-                if k == &node.key {
+                if key == &node.key {
                     true
-                } else if k < &node.key {
-                    Self::tree_contains(&node.left, k)
+                } else if key < &node.key {
+                    Self::tree_contains(&node.left, key)
                 } else {
-                    Self::tree_contains(&node.right, k)
+                    Self::tree_contains(&node.right, key)
                 }
             },
             None => false,
         }
     }
 
-    pub fn contains(&self, k: &T) -> bool {
-         let &Treap(ref n) = self;
-         Self::tree_contains(n, k)
+    /// Checks if a key exists in the treap.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// assert_eq!(t.contains(&0), false);
+    /// assert_eq!(t.contains(&1), true);
+    /// ```
+    pub fn contains(&self, key: &T) -> bool {
+         let &Treap(ref tree) = self;
+         Self::tree_contains(tree, key)
     }
 
-    fn tree_get<'a>(tree: &'a Tree<T, U>, k: &T) -> Option<&'a U> {
+    fn tree_get<'a>(tree: &'a Tree<T, U>, key: &T) -> Option<&'a U> {
         match *tree {
             Some(ref node) => {
-                if k == &node.key {
+                if key == &node.key {
                     Some(&node.value)
-                } else if k < &node.key {
-                    Self::tree_get(&node.left, k)
+                } else if key < &node.key {
+                    Self::tree_get(&node.left, key)
                 } else {
-                    Self::tree_get(&node.right, k)
+                    Self::tree_get(&node.right, key)
                 }
             }
             None => None,
         }
     }
 
-    pub fn get(&self, k: &T) -> Option<&U> {
+    /// Returns an immutable reference to the value associated with a particular key. It will
+    /// return `None` if the key does not exist in the treap.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// assert_eq!(t.get(&0), None);
+    /// assert_eq!(t.get(&1), Some(&1));
+    /// ```
+    pub fn get(&self, key: &T) -> Option<&U> {
         let &Treap(ref tree) = self;
-        Self::tree_get(tree, k)
+        Self::tree_get(tree, key)
     }
 
-    fn tree_get_mut<'a>(tree: &'a mut Tree<T, U>, k: &T) -> Option<&'a mut U> {
+    fn tree_get_mut<'a>(tree: &'a mut Tree<T, U>, key: &T) -> Option<&'a mut U> {
         match *tree {
             Some(ref mut node) => {
-                if k == &node.key {
+                if key == &node.key {
                     Some(&mut node.value)
-                } else if k < &node.key {
-                    Self::tree_get_mut(&mut node.left, k)
+                } else if key < &node.key {
+                    Self::tree_get_mut(&mut node.left, key)
                 } else {
-                    Self::tree_get_mut(&mut node.right, k)
+                    Self::tree_get_mut(&mut node.right, key)
                 }
             }
             None => None,
         }
     }
 
-    pub fn get_mut(&mut self, k: &T) -> Option<&mut U> {
+    /// Returns a mutable reference to the value associated with a particular key. Returns `None`
+    /// if such a key does not exist.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// *t.get_mut(&1).unwrap() = 2;
+    /// assert_eq!(t.get(&1), Some(&2));
+    /// ```
+    pub fn get_mut(&mut self, key: &T) -> Option<&mut U> {
         let &mut Treap(ref mut tree) = self;
-        Self::tree_get_mut(tree, k)
+        Self::tree_get_mut(tree, key)
     }
 
+    /// Returns the size of the treap.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// assert_eq!(t.size(), 1);
+    /// ```
     pub fn size(&self) -> usize {
         let &Treap(ref tree) = self;
         match *tree {
@@ -206,15 +315,15 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
-    fn tree_ceil<'a>(tree: &'a Tree<T, U>, k: &T) -> Option<&'a T> {
+    fn tree_ceil<'a>(tree: &'a Tree<T, U>, key: &T) -> Option<&'a T> {
         match *tree {
             Some(ref node) => {
-                if &node.key == k {
+                if &node.key == key {
                     Some(&node.key)
-                } else if &node.key < k {
-                    Self::tree_ceil(&node.right, k)
+                } else if &node.key < key {
+                    Self::tree_ceil(&node.right, key)
                 } else {
-                    let res = Self::tree_ceil(&node.left, k);
+                    let res = Self::tree_ceil(&node.left, key);
                     if res.is_some() {
                         res
                     } else {
@@ -226,20 +335,32 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
-    pub fn ceil(&self, k: &T) -> Option<&T> {
+    /// Returns a key in the treap that is greater than or equal to a particular key. Returns
+    /// `None` if such a key does not exist.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// assert_eq!(t.ceil(&0), Some(&1));
+    /// assert_eq!(t.ceil(&2), None);
+    /// ```
+    pub fn ceil(&self, key: &T) -> Option<&T> {
         let &Treap(ref tree) = self;
-        Self::tree_ceil(tree, k)
+        Self::tree_ceil(tree, key)
     }
 
-    fn tree_floor<'a>(tree: &'a Tree<T, U>, k: &T) -> Option<&'a T> {
+    fn tree_floor<'a>(tree: &'a Tree<T, U>, key: &T) -> Option<&'a T> {
         match *tree {
             Some(ref node) => {
-                if &node.key == k {
+                if &node.key == key {
                     Some(&node.key)
-                } else if &node.key > k {
-                    Self::tree_floor(&node.left, k)
+                } else if &node.key > key {
+                    Self::tree_floor(&node.left, key)
                 } else {
-                    let res = Self::tree_floor(&node.right, k);
+                    let res = Self::tree_floor(&node.right, key);
                     if res.is_some() {
                         res
                     } else {
@@ -251,9 +372,21 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
-    pub fn floor(&self, k: &T) -> Option<&T> {
+    /// Returns a key in the treap that is less than or equal to a particular key. Returns
+    /// `None` if such a key does not exist.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// assert_eq!(t.floor(&0), None);
+    /// assert_eq!(t.floor(&2), Some(&1));
+    /// ```
+    pub fn floor(&self, key: &T) -> Option<&T> {
         let &Treap(ref tree) = self;
-        Self::tree_floor(tree, k)
+        Self::tree_floor(tree, key)
     }
 
     fn tree_min(tree: &Tree<T, U>) -> Option<&T> {
@@ -269,6 +402,17 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
+    /// Returns the minimum key of the treap. Returns `None` if the treap is empty.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// t.insert(3, 3);
+    /// assert_eq!(t.min(), Some(&1));
+    /// ```
     pub fn min(&self) -> Option<&T> {
         let &Treap(ref tree) = self;
         Self::tree_min(tree)
@@ -287,11 +431,37 @@ impl<T: PartialOrd, U> Treap<T, U> {
         }
     }
 
+    /// Returns the maximum key of the treap. Returns `None` if the treap is empty.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// t.insert(3, 3);
+    /// assert_eq!(t.max(), Some(&3));
+    /// ```
     pub fn max(&self) -> Option<&T> {
         let &Treap(ref tree) = self;
         Self::tree_max(tree)
     }
 
+    /// Returns an iterator over the treap. The iterator will yield key-value pairs using in-order
+    /// traversal.
+    ///
+    /// # Examples
+    /// ```
+    /// use code::data_structures::Treap;
+    ///
+    /// let mut t = Treap::new();
+    /// t.insert(1, 1);
+    /// t.insert(3, 3);
+    /// let mut iterator = t.iter();
+    /// assert_eq!(iterator.next(), Some((&1, &1)));
+    /// assert_eq!(iterator.next(), Some((&3, &3)));
+    /// assert_eq!(iterator.next(), None);
+    /// ```
     pub fn iter(&self) -> TreapIterator<T, U> {
         let &Treap(ref tree) = self;
         TreapIterator { current: tree, stack: Vec::new() }
@@ -308,6 +478,9 @@ impl<'a, T: 'a + PartialOrd, U: 'a> IntoIterator for &'a Treap<T, U> {
 
 }
 
+/// An iterator for `Treap<T, U>`
+///
+/// This iterator traverses the elements of a treap in-order.
 pub struct TreapIterator<'a, T: 'a + PartialOrd, U: 'a> {
     current: &'a Tree<T, U>,
     stack: Vec<&'a Node<T, U>>,
