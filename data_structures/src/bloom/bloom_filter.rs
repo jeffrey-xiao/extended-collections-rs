56539df453170dcecb19d6ed95ab111a7eb7ca9a
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2018-03-21 20:01:08 -0700

    Update docs and tests

--- data_structures/src/bloom/bloom_filter.rs
+++ data_structures/src/bloom/bloom_filter.rs
@@ -4,6 +4,31 @@ use siphasher::sip::SipHasher;
 use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
 
+/// A space-efficient probabilistic data structure to test for membership in a set.
+///
+/// At its core, a bloom filter is a bit array, initially all set to zero. `K` hash functions
+/// map each element to `K` bits in the bit array. An element definitely does not exist in the
+/// bloom filter if any of the `K` bits are unset. An element is possibly in the set if all of the
+/// `K` bits are set. This particular implementation of a bloom filter uses two hash functions to
+/// simulate `K` hash functions. Additionally, it operates on only one "slice" in order to have
+/// predictable memory usage.
+///
+/// # Examples
+/// ```
+/// use data_structures::bloom::BloomFilter;
+///
+/// let mut filter = BloomFilter::new(100, 10);
+///
+/// assert!(!filter.contains(&"foo"));
+/// filter.insert(&"foo");
+/// assert!(filter.contains(&"foo"));
+///
+/// filter.clear();
+/// assert!(!filter.contains(&"foo"));
+///
+/// assert_eq!(filter.len(), 100);
+/// assert_eq!(filter.hasher_count(), 7);
+/// ```
 pub struct BloomFilter<T: Hash> {
     bit_vec: BitVec,
     hashers: [SipHasher; 2],
@@ -24,6 +49,15 @@ impl<T: Hash> BloomFilter<T> {
         ((bit_count as f64) / (item_count as f64) * 2f64.ln()).ceil() as usize
     }
 
+    /// Constructs a new, empty `BloomFilter<T>` with `bit_count` bits and an estimated max
+    /// capacity of `item_count` items.
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::bloom::BloomFilter;
+    ///
+    /// let filter: BloomFilter<u32> = BloomFilter::new(100, 10);
+    /// ```
     pub fn new(bit_count: usize, item_count: usize) -> Self {
         BloomFilter {
             bit_vec: BitVec::new(bit_count),
@@ -33,6 +67,15 @@ impl<T: Hash> BloomFilter<T> {
         }
     }
 
+    /// Constructs a new, empty `BloomFilter<T>` with an estimated max capacity of `item_count`
+    /// items and a maximum false positive probability of `fpp`.
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::bloom::BloomFilter;
+    ///
+    /// let filter: BloomFilter<u32> = BloomFilter::from_fpp(10, 0.01);
+    /// ```
     pub fn from_fpp(item_count: usize, fpp: f64) -> Self {
         let bit_count = (-fpp.log(2.0) * (item_count as f64) / 2f64.ln()).ceil() as usize;
         BloomFilter {
@@ -53,6 +96,16 @@ impl<T: Hash> BloomFilter<T> {
         ret
     }
 
+    /// Inserts an element into the bloom filter.
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::bloom::BloomFilter;
+    ///
+    /// let mut filter = BloomFilter::new(100, 10);
+    ///
+    /// filter.insert(&"foo");
+    /// ```
     pub fn insert(&mut self, item: &T) {
         let hashes = self.get_hashes(item);
         for index in 0..self.hasher_count {
@@ -63,28 +116,87 @@ impl<T: Hash> BloomFilter<T> {
         }
     }
 
+    /// Checks if an element is possibly in the bloom filter.
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::bloom::BloomFilter;
+    ///
+    /// let mut filter = BloomFilter::new(100, 10);
+    ///
+    /// assert!(!filter.contains(&"foo"));
+    /// filter.insert(&"foo");
+    /// assert!(filter.contains(&"foo"));
+    /// ```
     pub fn contains(&mut self, item: &T) -> bool {
         let hashes = self.get_hashes(item);
-        for index in 0..self.hasher_count {
+        (0..self.hasher_count).all(|index| {
             let mut offset = (index as u64).wrapping_mul(hashes[1]) % 0xffffffffffffffc5;
             offset = hashes[0].wrapping_add(offset);
             offset = offset % self.bit_vec.len() as u64;
-            if !self.bit_vec[offset as usize] {
-                return false;
-            }
-        }
-        true
+            self.bit_vec[offset as usize]
+        })
     }
 
+    /// Returns the number of bits in the bloom filter.
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::bloom::BloomFilter;
+    ///
+    /// let filter: BloomFilter<u32> = BloomFilter::new(100, 10);
+    ///
+    /// assert_eq!(filter.len(), 100);
+    /// ```
     pub fn len(&self) -> usize {
         self.bit_vec.len()
     }
 
+    /// Returns the number of hash functions used by the bloom filter.
+    ///
+    /// # Examples
+    /// ```
+    /// use data_structures::bloom::BloomFilter;
+    ///
+    /// let filter: BloomFilter<u32> = BloomFilter::new(100, 10);
+    ///
+    /// assert_eq!(filter.hasher_count(), 7);
+    /// ```
+    pub fn hasher_count(&self) -> usize {
+        self.hasher_count
+    }
+
+    /// Clears the bloom filter, removing all elements.
+    /// ```
+    /// use data_structures::bloom::BloomFilter;
+    ///
+    /// let mut filter = BloomFilter::new(100, 10);
+    ///
+    /// filter.insert(&"foo");
+    /// filter.clear();
+    ///
+    /// assert!(!filter.contains(&"foo"));
     pub fn clear(&mut self) {
         self.bit_vec.set_all(false)
     }
+}
 
-    pub fn hasher_count(&self) -> usize {
-        self.hasher_count
+#[cfg(test)]
+mod tests {
+    use super::BloomFilter;
+
+    #[test]
+    fn test_bloom_filter() {
+        let mut filter = BloomFilter::new(100, 10);
+
+        assert!(!filter.contains(&"foo"));
+        filter.insert(&"foo");
+        assert!(filter.contains(&"foo"));
+
+        filter.clear();
+        assert!(!filter.contains(&"foo"));
+
+        assert_eq!(filter.len(), 100);
+        assert_eq!(filter.hasher_count(), 7);
     }
 }
--- data_structures/src/bloom/mod.rs
+++ data_structures/src/bloom/mod.rs
@@ -1,2 +1,4 @@
 mod bloom_filter;
 mod scalable_bloom_filter;
+
+pub use self::bloom_filter::BloomFilter;
