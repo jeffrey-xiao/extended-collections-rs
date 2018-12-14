8bba9ffe5a0c113488cd2bde06dc77ff2ace10ab
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2018-12-14 01:07:27 -0500

    Update to nightly

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@
 //! [![Build Status](https://travis-ci.org/jeffrey-xiao/extended-collections-rs.svg?branch=master)](https://travis-ci.org/jeffrey-xiao/extended-collections-rs)
 //! [![codecov](https://codecov.io/gh/jeffrey-xiao/extended-collections-rs/branch/master/graph/badge.svg)](https://codecov.io/gh/jeffrey-xiao/extended-collections-rs)
 //!
-//! `extended-collections` contains various implementations of collections that are not found in the standard library.
+//! `extended-collections` contains various implementations of collections that are not found in the
+//! standard library.
 //!
 //! ## Usage
 //!
@@ -31,13 +32,14 @@
 //!  > Blelloch, Guy E., and Margaret Reid-Miller. 1998. “Fast Set Operations Using Treaps.” In *Proceedings of the Tenth Annual Acm Symposium on Parallel Algorithms and Architectures*, 16–26. SPAA ’98. New York, NY, USA: ACM. doi:[10.1145/277651.277660](https://doi.org/10.1145/277651.277660).
 //!  - [A Skip List Cookbook.](https://dl.acm.org/citation.cfm?id=93711)
 //!  > Pugh, William. 1990a. “A Skip List Cookbook.” College Park, MD, USA: University of Maryland at College Park.
-//!  - [Skip Lists: A Probabilistic Alternative to Balanced Trees](https://dl.acm.org/citation.cfm?id=78977)
+//!  - [Skip Lists: A Probabilistic Alternative to Balanced
+//!    Trees](https://dl.acm.org/citation.cfm?id=78977)
 //!  > Pugh, William. 1990b. “Skip Lists: A Probabilistic Alternative to Balanced Trees.” *Commun. ACM* 33 (6). New York, NY, USA: ACM: 668–76. doi:[10.1145/78973.78977](https://doi.org/10.1145/78973.78977).
 //!
 //! ## License
 //!
-//! `extended-collections-rs` is dual-licensed under the terms of either the MIT License or the Apache
-//! License (Version 2.0).
+//! `extended-collections-rs` is dual-licensed under the terms of either the MIT License or the
+//! Apache License (Version 2.0).
 //!
 //! See [LICENSE-APACHE](LICENSE-APACHE) and [LICENSE-MIT](LICENSE-MIT) for more details.
 
